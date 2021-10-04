1573028ff7ceeac8105baf6337ca3fe5d217b9fb
Carl Nolan <blackhxxdini@gmail.com>
2021-10-04 21:01:39 +0100

    Front-end completion

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,10 +1,8 @@
 import React, { useState, Fragment } from "react";
-import Image from "next/image";
-import testPic from "public/test.jpg";
-import { TrashIcon } from "@heroicons/react/solid";
+import { UserIcon } from "@heroicons/react/solid";
 import { Dialog, Transition } from "@headlessui/react";
 
-const ImageCard = () => {
+const Navbar = () => {
   let [isOpen, setOpen] = useState(false);
 
   const openModal = () => {
@@ -17,21 +15,23 @@ const ImageCard = () => {
 
   return (
     <>
-      <div className="w-full h-full relative overflow-hidden">
-        <Image
-          alt="image_alt"
-          src={testPic}
-          placeholder="blur"
-          objectFit="cover"
-          className="rounded hover:shadow-2xl object-cover"
-        />
-        <div className="absolute w-full h-full px-2.5 py-2.5 bottom-0 inset-x-0 bg-transparent cursor-pointer text-opacity-0 hover:text-opacity-100 text-white text-xs md:text-base text-center truncate leading-4 transition duration-300">
-          <p className="font-thin">Image Name here</p>
+      <div className="flex flex-wrap md:justify-between w-full p-6">
+        <div className="flex w-full md:w-3/4 lg:w-3/5 space-x-4">
+          <div className="flex my-auto space-x-2 font-bold">
+            <UserIcon className="w-6 h-6" />
+            <p className="tracking-tight">MyUnsplash</p>
+          </div>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:ring-2 focus:ring-blue-200 focus:shadow-outline focus:outline-none focus:bg-white focus:border-blue-500"
+            placeholder="Search by name"
+          />
+        </div>
+        <div className="flex ml-4 mt-2 md:mt-0">
           <button
             onClick={openModal}
-            className="rounded-xl appearance-none float-right p-2 hover:bg-red-400"
+            className="bg-green-500 hover:bg-green-700 focus:ring-4 focus:ring-green-200 text-white tracking-tight font-semibold py-2 px-4 shadow-lg rounded-lg transition duration-300"
           >
-            <TrashIcon className="w-6 h-6" />
+            Add a photo
           </button>
         </div>
       </div>
@@ -69,8 +69,15 @@ const ImageCard = () => {
               leaveTo="opacity-0 scale-95"
             >
               <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-                <Dialog.Title className="font-bold tracking-tight">Are you sure?</Dialog.Title>
+                <Dialog.Title className="font-bold tracking-tight">
+                  Add a Photo
+                </Dialog.Title>
                 <div className="flex flex-col mt-2 space-y-2">
+                  <label className="font-thin tracking-tight">Label</label>
+                  <input
+                    placeholder="Lorem ipsum dolor.."
+                    className="shadow bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                  />
                   <label className="font-thin tracking-tight">Photo URL</label>
                   <input
                     placeholder="https://image.unsplash/90384"
@@ -87,9 +94,9 @@ const ImageCard = () => {
                   </button>
                   <button
                     type="button"
-                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-red-900 shadow bg-red-100 border border-transparent rounded-md hover:bg-red-200 focus:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-red-500"
+                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-green-900 shadow bg-green-100 border border-transparent rounded-md hover:bg-green-200 focus:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-green-500"
                   >
-                    Delete
+                    Submit
                   </button>
                 </div>
               </div>
@@ -101,4 +108,4 @@ const ImageCard = () => {
   );
 };
 
-export default ImageCard;
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { UserIcon } from "@heroicons/react/solid";
+import Navbar from "@/components/layout/navbar";
 import ImageCard from "@/components/image/imageCard";
 
 const Home: NextPage = () => {
@@ -10,23 +10,7 @@ const Home: NextPage = () => {
         <title>My Unsplash</title>
       </Head>
       <div className="flex flex-col min-h-screen justify-between p-6">
-        <div className="flex flex-wrap md:justify-between w-full p-6">
-          <div className="flex w-full md:w-3/4 lg:w-3/5 space-x-4">
-            <div className="flex my-auto space-x-2 font-bold">
-              <UserIcon className="w-6 h-6" />
-              <p>MyUnsplash</p>
-            </div>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:ring-2 focus:ring-blue-200 focus:shadow-outline focus:outline-none focus:bg-white focus:border-blue-500"
-              placeholder="Search by name"
-            />
-          </div>
-          <div className="flex ml-4 mt-2 md:mt-0">
-            <button className="bg-green-500 hover:bg-green-700 focus:ring-4 focus:ring-green-200 text-white font-semibold py-2 px-4 rounded transition duration-300">
-              Add a photo
-            </button>
-          </div>
-        </div>
+        <Navbar />
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
           {[...Array(10)].map((e, i) => (
             <ImageCard key={e} />
